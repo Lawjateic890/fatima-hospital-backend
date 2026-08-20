5dc2a25741ed44848e24a9fcfbc023f9ca5f0d18
Ihtisham Ahmed <ihtishamahmed930@gail.com>
2026-08-20 19:41:26 +0500

    Debug Railway env

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME
-});
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 console.log("__dirname =", __dirname);
 const express = require("express");
 const cors = require("cors");
