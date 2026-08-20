844d1b675f299e513b82010d0491366e09758017
Ihtisham Ahmed <ihtishamahmed930@gail.com>
2026-08-20 18:44:29 +0500

    Debug Railway env

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 const pool = require("./config/db");
 
