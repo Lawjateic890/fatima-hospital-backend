8b9786510e44ab46d8e09d736911f6b9257b7fdc
Ihtisham Ahmed <ihtishamahmed930@gail.com>
2026-08-20 18:51:16 +0500

    Debug database env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 const { Pool } = require("pg");
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
